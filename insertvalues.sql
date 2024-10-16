c9e42a11512e463c14428576d821933a4f6736bb
remiab <92585639+remiab@users.noreply.github.com>
2024-10-16 12:28:49 +0100

    Create insertvalues.sql

---
+++ insertvalues.sql
@@ -0,0 +1,15 @@
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Pushpak',1);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Nayan',2);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Chait',3);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rutuja',4);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Tanmay',5);
+
+INSERT INTO team5 (firstname, rollnumber) VALUES ('Ewan',1);
+INSERT INTO team5 (firstname, rollnumber) VALUES ('John',2);
+INSERT INTO team5 (firstname, rollnumber) VALUES ('Paul',3);
+INSERT INTO team5 (firstname, rollnumber) VALUES ('Remi',4);
+
+INSERT INTO foods (food, rollnumber) VALUES ('pizza',1);
+INSERT INTO foods (food, rollnumber) VALUES ('chicken',2);
+INSERT INTO foods (food, rollnumber) VALUES ('beef',3);
+INSERT INTO foods (food, rollnumber) VALUES ('chocolate',4);
