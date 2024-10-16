5fe512124951f8b6bffbf7dc38c08afa075510ef
remiab <92585639+remiab@users.noreply.github.com>
2024-10-16 12:28:01 +0100

    Create createtables.sql

---
+++ createtables.sql
@@ -0,0 +1,3 @@
+CREATE TABLE priarworld ( firstname varchar(20), rollnumber int(5));
+CREATE TABLE team5 ( firstname varchar(20), rollnumber int(5));
+CREATE TABLE foods ( food varchar(20), rollnumber int(5));
